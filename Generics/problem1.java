53f987a7918403c0c477e7eea4597981e3408fc9
SomsubhroChakraborty <somsubhrochakraborty07@gmail.com>
2025-01-20 07:27:29 +0530

    comments added

--- Generics/problem1.java
+++ Generics/problem1.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 
 
 public class problem1 {
-    public static <T extends Comparable <T>> void sort(T[] arr){
+    public static <T extends Comparable <T>> void sort(T[] arr){ //comparable is a class where compareTo belongs
         for(int i=0; i<=arr.length-1;i++){
             for(int j=0;j<arr.length-j-1;j++){
-                if(arr[j].compareTo(arr[j+1])>0){
+                if(arr[j].compareTo(arr[j+1])>0){ //compareTO use for sorting
                     T temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
